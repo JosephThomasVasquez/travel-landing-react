101d8b74b684f7e9da1e375942ea61ec855076dd
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-08 17:39:58 -0700

    Created NavBar component and imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <h1 className="text-blue-500">Travel</h1>
       </header>
