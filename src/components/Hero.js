1da7e457f32ba7c6725be4a51859654cfc47a7e8
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-09 09:51:24 -0700

    Added hero image to hero state and img element

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import heroImage from "../images/pexels-artem-beliaikin-2106776.jpg";
 
 const Hero = () => {
-  const [herImg, setHeroImg] = useState({
-    img: "",
+  const [heroImg, setHeroImg] = useState({
+    img: heroImage,
   });
 
-  return <div></div>;
+  return (
+    <div>
+      <img src={heroImg.img} alt="" />
+    </div>
+  );
 };
 
 export default Hero;
