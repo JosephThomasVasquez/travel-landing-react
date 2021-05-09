7c0f6d8a03a228c9709a94d6ea7c253b92d2aa48
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-08 17:34:14 -0700

    Removed starter code and styles from create react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Contains Travel text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Travel/i);
   expect(linkElement).toBeInTheDocument();
 });
