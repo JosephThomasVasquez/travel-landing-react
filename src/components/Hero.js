f759dfa25b8f86be44e5c77e00098eb8c3cd4545
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-09 09:28:28 -0700

    Added Hero componenet to control Hero image assets

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Hero = () => {
+  const [herImg, setHeroImg] = useState({
+    img: "",
+  });
+
+  return <div></div>;
+};
+
+export default Hero;
