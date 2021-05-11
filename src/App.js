a4322a42d3293c49138d560a31f2951a77171c80
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-10 20:46:39 -0700

    Added AboutSection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import AboutSection from "./components/AboutSection";
 
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
+      <AboutSection />
     </>
   );
 }
---
+++ src/components/AboutSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <section className="grid grid-rows-2 grid-cols-2 grid-flow-col gap-4 pt-32">
+      <div className="row-span-6 col-span-2 bg-gray-400 h-96 block font-serif font-bold text-gray-800 lg:text-6xl md:text-5xl sm:text-3xl">
+        Explore All Corners Of The World With Us
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
