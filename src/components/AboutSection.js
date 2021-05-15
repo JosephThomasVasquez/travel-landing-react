1dd269704008b3e068fcf7861779483128866de6
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-05-15 09:37:28 -0700

    Changed all sm breakpoints to the new cs breakpoint

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -65,41 +65,41 @@ const AboutSection = () => {
     <section className="">
       <div className="grid grid-rows-2 grid-cols-12 gap-8 w-screen">
         <div
-          className="md:col-start-1 md:col-end-7 sm:col-span-12 ... h-96 h-auto"
+          className="md:col-start-1 md:col-end-7 xs:col-span-12 ... h-96 h-auto"
           ref={addRefs}
         >
-          <div className="block font-mono font-bold text-primary-orange lg:text-2xl md:text-5xl sm:text-3xl mb-8 pl-16 ">
+          <div className="block font-mono font-bold text-primary-orange lg:text-2xl md:text-5xl xs:text-3xl mb-8 pl-16 ">
             ABOUT US
           </div>
-          <div className="block font-serif font-bold text-gray-800 lg:text-6xl md:text-5xl sm:text-3xl px-16">
+          <div className="block font-serif font-bold text-gray-800 lg:text-6xl md:text-5xl xs:text-3xl px-16">
             Explore All Corners Of The World With Us
           </div>
         </div>
 
         <div
-          className="md:col-start-7 md:col-end-13 sm:col-span-12 ... bg-primary-orange-100 h-96 p-16 h-auto"
+          className="md:col-start-7 md:col-end-13 xs:col-span-12 ... bg-primary-orange-100 h-96 p-16 h-auto"
           ref={addRefs}
         >
-          <div className="block font-bold text-gray-700 lg:text-2xl md:text-1xl sm:text-1xl mb-8 pl-8">
+          <div className="block font-bold text-gray-700 lg:text-2xl md:text-1xl xs:text-1xl mb-8 pl-8">
             We enjoy creating a variety of destinations and creating experience
             packages so customers can always find something new and exciting or
             just a change of view for relaxation.
           </div>
-          <div className="block font-serif font-bold text-gray-800 lg:text-2xl md:text-2xl sm:text-2xl pl-8">
+          <div className="block font-serif font-bold text-gray-800 lg:text-2xl md:text-2xl xs:text-2xl pl-8">
             <button className="font-semibold bg-primary-orange text-gray-100 inline-block p-6 px-10 hover:shadow-lg hover:bg-primary-orange-50 hover:text-gray-100 transition-all duration-500 ease-in-out">
               Learn More
             </button>
           </div>
         </div>
 
-        <div className="md:col-start-1 md:col-end-7 sm:col-span-12 ...">
+        <div className="md:col-start-1 md:col-end-7 xs:col-span-12 ...">
           <div className="" ref={addRefs}>
             <img src={aboutImage2} alt="" />
           </div>
         </div>
 
         <div
-          className="md:col-start-7 md:col-end-13 sm:col-span-12 ... bg-sky-blue shadow-lg static"
+          className="md:col-start-7 md:col-end-13 xs:col-span-12 ... bg-sky-blue shadow-lg static"
           ref={addRefs}
         >
           <div className="rounded overflow-hidden">
